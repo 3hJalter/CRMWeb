aa4a0667be78f5d6c4c2bdfc7a50d8947209b7e7
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2022-09-15 09:25:10 +0700

    Create "Update Customer" href for Update service (3/4 of CRUD)

--- crm-web/src/main/java/com/hoang/springdemo/controller/CustomerController.java
+++ crm-web/src/main/java/com/hoang/springdemo/controller/CustomerController.java
@@ -5,11 +5,9 @@ import com.hoang.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.jws.WebParam;
 import java.util.List;
 
 @Controller
@@ -35,6 +33,16 @@ public class CustomerController {
     public String showFormForAdd(Model model) {
         Customer customer = new Customer();
         model.addAttribute("customer", customer);
+        return "customer-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int id, Model model) {
+        // get the customer from the database
+        Customer customer = customerService.getCustomer(id);
+        // set customer as a model attribute to pre- populate the form
+        model.addAttribute("customer", customer);
+
         return "customer-form";
     }
 
